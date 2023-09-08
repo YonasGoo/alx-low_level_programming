8f6b80c37433a4eacdf03c329e9fcebd315bd439
pwntheweb <ezra005@proton.me>
2023-09-08 01:32:29 +0000

    first time C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -Enrty point
+ *
+ * Retrun: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
