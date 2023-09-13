f68d939de12710403e22fe309e1e8a3f64584b2d
pwntheweb <ezra005@proton.me>
2023-09-13 19:24:52 +0000

    added

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,12 @@
 
 void jack_bauer(void)
 {
-	for (int h = 0; h < 24; h++)
+	char h;
+	char m;
+
+	for (h = 0; h < 24; h++)
 	{
-		for (int m = 0; m < 60; m++)
+		for (m = 0; m < 60; m++)
 		{
 			_putchar('0' + (h / 10));
 			_putchar('0' + (h % 10));
@@ -20,5 +23,4 @@ void jack_bauer(void)
 			_putchar('\n');
 		}
 	}
-	return (0);
 }
