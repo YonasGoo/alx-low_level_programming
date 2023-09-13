ef47be4a6fbb5d48e77148ff568ab93d635a2b07
pwntheweb <ezra005@proton.me>
2023-09-13 18:04:58 +0000

    added

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - main function
+ *
+ * @n: checks the sign of the number
+ *
+ * Return: 1 if postitive, 0 if zero, -1 if negative
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1):
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
