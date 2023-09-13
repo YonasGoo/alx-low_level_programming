43a9ab4653d0d0c3ee6c7432faae64379578f4d8
pwntheweb <ezra005@proton.me>
2023-09-13 17:58:26 +0000

    added

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- * _isalpha(int c) - main function
+ * _isalpha - main function
  *
- * Return: return 1 if letter, return 0 otherwise
+ * @c: character to compare
  *
- * @c - character
+ * Return: 1 if true, 0 if false
+ *
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
