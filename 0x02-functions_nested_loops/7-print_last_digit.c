c9e6d8287241a732dc82b813f2d03fa3f360bc62
pwntheweb <ezra005@proton.me>
2023-09-13 18:49:34 +0000

    added

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_last_digit
+ * print_last_digit - main function
  * @n: the number to be evaluated
  *
  * Return: alwyas 0
@@ -12,6 +12,5 @@ int print_last_digit(int n)
 	int lastDigit = n % 10;
 
 	_putchar('0' + lastDigit);
-	_putchar('\n');
 	return (0);
 }
