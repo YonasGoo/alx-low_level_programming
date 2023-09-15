3c3206de2085e31535b20a97c42267ded5290543
pwntheweb <ezra005@proton.me>
2023-09-15 18:30:56 +0000

    big fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,23 +1,26 @@
 #include "main.h"
 /**
- * print_square - prints square 
+ * print_square - prints square
  *
- * @size: var to set square size
+ * @size: integer to set square size
  */
-
 void print_square(int size)
 {
-	int i, j;
+	int i;
+	int j;
 
 	if (size <= 0)
-		_putchar('\n');
-
-	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	} else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
