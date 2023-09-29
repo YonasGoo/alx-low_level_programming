eeefe0767092739800ea1aa45702336886a59b89
pwntheweb <ezra005@proton.me>
2023-09-29 19:23:46 +0000

    another

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,11 +12,11 @@ int _pow_recursion(int x, int y)
 	{
 	return (1);
 	}
-	if (y > 0)
+	else if (y > 0)
 	{
 	return (x * _pow_recursion(x, y - 1));
 	}
-	if (y < 1)
+	else (y < 1)
 	{
 	return (-1);
 	}
