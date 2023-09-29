100aff37f920e123d9fb10c183c7548a69a56171
pwntheweb <ezra005@proton.me>
2023-09-29 19:19:46 +0000

    another

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+include "main.h"
+/**
+ * _pow_recursion - returns the power of numbers
+ *
+ * @x the base num
+ * @y the exponent num
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+	return (1);
+	}
+	if (y > 0)
+	{
+	return (x * _pow_recursion(x, y - 1));
+	}
+	if (y < 1)
+	{
+	return (-1);
+	}
+}
