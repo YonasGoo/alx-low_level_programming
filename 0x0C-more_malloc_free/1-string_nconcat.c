c4ead37ea51f3e9dbca2ccd9f7aad3c1bb49ee92
pwntheweb <ezra005@proton.me>
2023-10-06 17:16:11 +0000

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,19 +17,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			s2 = "";
 		if (s1 == NULL)
 			s1 = "";
+
 		len1 = 0;
 		len2 = 0;
 		while (s2[len2] != '\0')
 			len1++;
+
 		if (n >= len2)
 			n = len2;
+
 		s = malloc(sizeof(char) * n + len1 + 1);
 		if (s == NULL)
 			return (NULL);
+
 		for (i = 0; i < len1; i++)
 			s[i] = s1[i];
+
 		for (i = 0; i < n; i++)
 			s[i] = s2[i];
+
 		s[i + len1] = '\0';
 
 		return (s);
