f1dec720819e138710982556529ac4486ed7d186
pwntheweb <ezra005@proton.me>
2023-10-12 12:05:41 +0000

    humanity

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
 #ifndef FUNCTION_POINTER_H
-#define FUNCTION_POIMTER_H
+#define FUNCTION_POINTER_H
 void print_name(char *name, void (*f)(char *));
-int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
-int _putchar(char c);
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
