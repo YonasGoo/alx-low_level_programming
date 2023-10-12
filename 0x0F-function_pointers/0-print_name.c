fc325e0836769b242f3a3f0058e7030b5ff1db86
pwntheweb <ezra005@proton.me>
2023-10-12 12:02:46 +0000

    we all need mirror to remind ourselves who we are

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "function_pointers.h"
+#include <stdio.h>
 /**
- * print_name - print the name
- * @name: name to print
- * @f: function use
+ * print_name - function that print a name
+ * @name: first value
+ * @f: function
+ *
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,22 +1,24 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "function_pointers.h"
+#include <stdio.h>
 /**
- * int_index - indexes array i
- * @array: array to go
- * @size: size of array
- * @cmp: function to do something with size
- * Return: int value
+ * int_index - function that searches an integer
+ * @array: first value
+ * @size: function
+ * @cmp: function
+ *
+ * Return: Value
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int value = 0, i;
 
-	if (size <= 0 || array == NULL || cmp == NULL)
-		return (1);
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))
+		value = cmp(array[i]);
+		if (value == 1)
 			return (i);
 	}
 	return (-1);
