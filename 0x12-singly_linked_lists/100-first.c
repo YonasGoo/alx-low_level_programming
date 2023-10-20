8aa143b2648b8f6bb2850535674571493312541f
pwntheweb <ezra005@proton.me>
2023-10-20 01:26:11 +0000

    advanced

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *
- *
+ * startup_fun - A function exectuted before the main function
+ * This function is decorated with the __attribute__((constructor)) attribute
  */
 void startup_fun(void) __attribute__((constructor));
 
@@ -9,7 +9,11 @@ void startup_fun(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
-
+/**
+ * main - The entry point 
+ *
+ * Return: Always return 0 (Sucess)
+ */
 int main()
 {
 	return (0);
