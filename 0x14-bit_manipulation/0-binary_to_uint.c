118f83d7afaa53e53b63af0d0a8f70419e0947fd
pwntheweb <ezra005@proton.me>
2023-10-26 13:38:43 +0000

    some fixes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,18 +10,16 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 
 	unsigned int result = 0;
+	int i = 0;
 
-	if (b == NULL)
-		return (0);
-	while (*b)
+	while (b[i] != '\0')
 	{
-		if (*b == '1')
-			result = (result << 1) | 1;
-		else if (*b == '0')
-			result <<= 1;
-		else
+		if (b[i] != '0' && b[i] '1')
+		{
 			return (0);
-		b++
+		}
+		result = (result << 1) | (b[i] - '0');
+		i++;
 	}
 	return (result);
 }
